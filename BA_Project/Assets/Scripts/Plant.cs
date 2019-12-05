2820186f3233917cb1add992409737106080bf04
Tonio Suessdorf <tonio.suessdorf@gmail.com>
2019-12-05 16:40:18 +0100

    WIP Added scripts: Environment, Spirit, Plant, Tree, Mushroom, WarningLowWater

---
+++ BA_Project/Assets/Scripts/Plant.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    enum WaterSupply
+    {
+        Low,
+        Normal,
+        high
+    }
+
+    enum NutriantSupply
+    {
+        Low,
+        Normal,
+        high
+    }
+    enum Danger
+    {
+        LowWater,
+        LowNutrients,
+        Animals,
+        Insects
+    }
+
+    [SerializeField] WaterSupply _waterSupply;
+    [SerializeField] NutriantSupply _nutrientSupply;
+    [SerializeField] bool _isHealthy;
+    [SerializeField] bool _hasEnergy;
+    [SerializeField] Danger[] _dangers;
+
+    public void CommunicateViaRoots()
+    {
+
+    }
+
+    public void CommunicateViaFume()
+    {
+
+    }
+}
